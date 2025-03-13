58ad18541ce36f01957cb0230385e87a946a6165
Munyangeyo Elie <munyangeyoelie38@gmail.com>
2025-03-13 19:11:04 +0200

    addtion of somm dist

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// Get the environment variables
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+// Ensure that the environment variables are defined, otherwise throw an error
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase URL or anonymous key is missing from environment variables');
+}
+
+// Create the Supabase client
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url'
 
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/financefin/",
+  plugins: [vue(), vueDevTools(), tailwindcss()],
+  
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   build: {
     chunkSizeWarningLimit: 1000, // 1000 KB (1 MB)
   },
-});
+})
